0aecfc5042896cd6b0009720d0c537c8a106c76d
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2023-12-29 10:06:12 -0700

    Add FastAPI backend and React client files

--- projects/shining-spectrum/src/components/SalesReportTable.tsx
+++ projects/shining-spectrum/src/components/SalesReportTable.tsx
@@ -21,12 +21,22 @@ const SalesReport = () => {
     return (
         <div>
             <h1>Sales Report</h1>
-            {salesData.map((item, index) => (
-                <div key={index}>
-                    <p>{item.model_type}</p>
-                    <p>{item.report}</p>
-                </div>
-            ))}
+            <table className={"table-auto"}>
+                <thead>
+                    <tr>
+                        <th className={"text-2xl"}>Model Type</th>
+                        <th className={"text-2xl"}>Report</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {salesData.map((item, index) => (
+                        <tr key={index}>
+                            <td className={'px-2 text-xl'}>{item.model_type}</td>
+                            <td className={'py-7 text-lg'}>{item.report}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
